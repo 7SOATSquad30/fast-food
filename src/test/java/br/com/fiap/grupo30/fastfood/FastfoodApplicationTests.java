dd8b69160c9eb4ed7558b828d6def0f0ff310bad
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-04-25 21:36:52 -0300

    first commit

---
+++ src/test/java/br/com/fiap/grupo30/fastfood/FastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.grupo30.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
