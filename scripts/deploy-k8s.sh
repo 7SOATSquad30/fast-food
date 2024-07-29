6b1183d978f747390316be35d4fb4ffeb1e6e0a2
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-07-29 01:50:15 -0300

    add more to k8s script

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -2,6 +2,7 @@
 kubectl apply -f ./k8s/metrics.yaml
 
 # database
+kubectl create configmap cm-init --from-file=init.sql
 kubectl apply -f ./k8s/configmap.yaml
 kubectl apply -f ./k8s/postgres-pv.yaml
 kubectl apply -f ./k8s/postgres-pvc.yaml
@@ -12,3 +13,6 @@ kubectl apply -f ./k8s/postgres-deployment.yaml
 kubectl apply -f ./k8s/api-svc.yaml
 kubectl apply -f ./k8s/api-hpa.yaml
 kubectl apply -f ./k8s/api-deployment.yaml
+
+# application port forward (temp)
+# kubectl port-forward -n default service/api-svc 30007:80
