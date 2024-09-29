7bb3e03eb6e3712b32fa5592c142690a988bd40e
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-09-28 23:30:57 -0300

    pipe: deploy to k8s cluster

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -1,15 +1,3 @@
-# metric server
-kubectl apply -f ./k8s/metrics.yaml
-
-# database
-kubectl create configmap cm-init --from-file=init.sql
-kubectl apply -f ./k8s/configmap.yaml
-kubectl apply -f ./k8s/postgres-pv.yaml
-kubectl apply -f ./k8s/postgres-pvc.yaml
-kubectl apply -f ./k8s/postgres-svc.yaml
-kubectl apply -f ./k8s/postgres-deployment.yaml
-
-# application
 kubectl apply -f ./k8s/api-svc.yaml
 kubectl apply -f ./k8s/api-hpa.yaml
 kubectl apply -f ./k8s/api-deployment.yaml
