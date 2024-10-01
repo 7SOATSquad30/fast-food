3017a8a8fb02bf7e68df0b89300c12914d8a43dc
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-10-01 02:15:04 -0300

    feat: config prod and local servers on swagger ui

--- src/main/java/br/com/fiap/grupo30/fastfood/FastfoodApplication.java
+++ src/main/java/br/com/fiap/grupo30/fastfood/FastfoodApplication.java
@@ -1,9 +1,18 @@
 package br.com.fiap.grupo30.fastfood;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "My API", version = "v1"),
+        servers = {
+            @Server(url = "http://localhost:8080", description = "Local Development"),
+            @Server(url = "https://29glms05ff.execute-api.us-east-1.amazonaws.com", description = "Production Server")
+        })
 public class FastfoodApplication {
 
     public static void main(String[] args) {
